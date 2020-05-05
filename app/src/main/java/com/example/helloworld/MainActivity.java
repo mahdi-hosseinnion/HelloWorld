240c4e7527f9063e7cbadc545852b4bb2ee74bf4
mahdi <ssmmhh1382@gmail.com>
2020-05-05 14:08:49 +0330

    add Text

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //set Hello World
+        TextView txt=findViewById(R.id.txt_mainText);
+        txt.setText("Hello World! yEAAAA");
     }
 }
